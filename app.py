95a0d5c3c98af14fea4ef255dfd49dfaa203fb76
sharontang2001 <70727976+sharontang2001@users.noreply.github.com>
2021-12-02 05:32:21 +0000

    Thu, Dec 2, 2021, 5:32 AM +00:00

--- app.py
+++ app.py
@@ -54,17 +54,14 @@ def diary():
 
         # Define variables
         entry = request.form.get("entry")
-        name = request.form.get("name")
         
         # Validate form submission
-        if not name:
-            return apology("missing name")
-        elif not entry:
+        if not entry:
             return apology("missing entry")
         elif len(entry) > 500:
             return apology("keep diary entries under 500 characters")
 
-        diary = db.execute("INSERT INTO diary (user_ID, name, entry) VALUES(?, ?, ?)", user_id, name, entry)
+        diary = db.execute("INSERT INTO diary (user_ID, entry) VALUES(?, ?)", user_id, entry)
 
         session["user_id"] = id
         return redirect("/")
