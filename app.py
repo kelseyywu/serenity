d8f8a15ae77063e66c2ce01c2314d1ea0f2136df
Kelsey Wu <kelseywu@college.harvard.edu>
2021-12-02 21:56:03 +0000

    Thu, Dec 2, 2021, 9:56 PM +00:00

--- app.py
+++ app.py
@@ -88,9 +88,9 @@ def quiz():
     if request.method == "POST":
         emotion_list = request.form.getlist('emotions')
         emotion_string = ",".join(emotion_list)
-        return redirect("/")
+        return render_template("quizzed.html", emotion_string=emotion_string)
     else:
-        return render_template("quiz.html", emotion_string=emotion_string)
+        return render_template("quiz.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
