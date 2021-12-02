816b22584147cdb0abae321f7e996df98a2034b1
sharontang2001 <70727976+sharontang2001@users.noreply.github.com>
2021-12-02 04:09:11 +0000

    Thu, Dec 2, 2021, 4:09 AM +00:00

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-import pandas as pd
 
 from helpers import apology, login_required
 
